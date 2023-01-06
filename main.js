327ddfc19ed42c396109a3c0652f0f24d7272f55
Nardics <bmo230@uky.edu>
2023-01-05 21:13:57 -0600

    Update main.js

--- main.js
+++ main.js
@@ -23,7 +23,12 @@ function addInteraction(){
     const eventLatLng = [[-82,127], [-57, 327],[-263, 708], [-255, 588], [-317, 255], [-133, 641], [-310, 430], [-295, 323]]
     const eventZoom   = [2, 2, 3, 3, 3, 3, 3, 3]
     
-    const desc = [`Text for the first page.`,
+    const desc = [`The cartographer Chase constructed his map some five years after the conclusion of WWII in 1945, and the formation
+    of the United Nations. <a href="https://www.archives.gov/milestone-documents/united-nations-charter">The United Nations </a>itself was formed
+    in response to what America and her allies considered the Axis Powers. These were namely German, Italy and Japan.
+    <p> Througout this period and beyond, there has been several episodes in World History which play on the played on the 
+    theme of "Us vs Them", as it relates to differing political ideologies. The map author chooses to employ <a href = "https://en.wikipedia.org/wiki/Yin_and_yang"> Yin and Yang</a> to symbolise
+    these tensions that would continue to orient the rest of the world to the ideological battles that would continue to play out.`,
 
         `<h3>Title for second page</h3><p>Text for second page</p>`,
         `<h3>Page 3 title</h3><p>Page 3 content</p>`,
@@ -32,10 +37,7 @@ function addInteraction(){
         `<h3>Page 5 title</h3><p>Page 5 content.</p>`,
         `<h3>Page 6 title</h3><p>Page 6 content</p>`,
         `<h3>Page 7 title</h3><p>Page 7 content</p>`,
-        // `This map reveals a snapshot in time of New England’s complex history and relationship between the colonists, 
-        // indigenous communities, and the land.  <a href = “https://collections.leventhalmap.org/exhibits/25”> 
-        // The Leventhal Map Center Collection online exhibition ‘America Transformed’ provides additional materials to 
-        // learn more about the history of land dispossession in the United States</a>.`
+        
     ];
 
     document
